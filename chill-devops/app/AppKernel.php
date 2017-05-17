43ca497535e13a379c17ac1628d01d5a3e57a3e2
Nicolas Ferec <nicolasferec@outlook.com>
2017-05-17 13:56:10 +0200

    Fix Mac

--- chill-devops/app/AppKernel.php
+++ chill-devops/app/AppKernel.php
@@ -47,4 +47,8 @@ class AppKernel extends Kernel
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
+    public function __construct($environment, $debug) {
+        date_default_timezone_set('Europe/Paris');
+        parent::__construct($environment, $debug);
+    }
 }
