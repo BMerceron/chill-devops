52455c52f7385e7529b407c6f447189373961d0d
Bastoon <bastien.merceron@gmail.com>
2017-06-13 16:08:02 +0200

    add label for name

--- chill-devops/src/AppBundle/Form/ScenarioType.php
+++ chill-devops/src/AppBundle/Form/ScenarioType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ScenarioType extends AbstractType
 {
@@ -13,7 +14,10 @@ class ScenarioType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name');
+        $builder->add('name', TextType::class,
+            array(
+                'label' => 'Nom du Scénario'
+            ));
     }
     
     /**
