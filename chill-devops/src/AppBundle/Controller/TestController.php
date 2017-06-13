d9747a3218b56e7968af213f6a3e262d1b19f025
jonathandodard <jonathan.dodard@gmail.com>
2017-06-13 09:13:26 +0200

    create bundle export pdf

--- chill-devops/src/AppBundle/Controller/TestController.php
+++ chill-devops/src/AppBundle/Controller/TestController.php
@@ -6,11 +6,34 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+
 class TestController extends Controller
 {
     public function testAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('AppBundle::test.html.twig');
+    }
+
+
+    /**
+     * Export to PDF
+     *
+     * @Route("/pdf", name="acme_demo_pdf")
+     */
+    public function pdfAction()
+    {
+        $html = $this->renderView('AppBundle::testPdf.html.twig');
+
+        $filename = sprintf('test-%s.pdf', date('Y-m-d'));
+
+        return new Reponse(
+            $this->get('knp_snappy.pdf')->getOutputFromHtml($html),
+            200,
+            [
+                'Content-Type'        => 'application/pdf',
+                'Content-Disposition' => sprintf('attachment; filename="%s"', $filename),
+            ]
+        );
     }
 }
