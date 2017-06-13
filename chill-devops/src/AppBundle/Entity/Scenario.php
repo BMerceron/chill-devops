7f7787651324948f098f27d4664d5bce2d7cd6ee
Bastoon <bastien.merceron@gmail.com>
2017-06-13 15:20:53 +0200

    fix scenario entity energycost

--- chill-devops/src/AppBundle/Entity/Scenario.php
+++ chill-devops/src/AppBundle/Entity/Scenario.php
@@ -69,7 +69,7 @@ class Scenario
     /**
      * @var int
      *
-     * @ORM\Column(name="energyCost", type="integer", nullable=true
+     * @ORM\Column(name="energyCost", type="integer", nullable=true)
      */
     private $energyCost;
 
