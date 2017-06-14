4a6380c08eff52c61a687fd28851d1dca9ecdbc1
Nicolas Ferec <nicolasferec@outlook.com>
2017-06-14 14:48:07 +0200

    Add gitignore and work on app

--- server-api/app.js
+++ server-api/app.js
@@ -1,27 +1,8 @@
-var http = require('http');
-var url = require('url');
-var querystring = require('querystring');
-var sys = require('sys')
-var exec = require('child_process').exec;
+var io = require('socket.io-client')('http://127.0.0.1:9090');
 
-var server = http.createServer(function(req, res) {
-	var params = querystring.parse(url.parse(req.url).query);
-	var page = url.parse(req.url).pathname;
-	if(req.method === 'GET' && page === '/simulate'){
-		res.writeHead(200, {"Content-Type": "application/json"});
-		exec("echo test", function(err, stdout, stderr){
-			res.end(stdout);
-		});
-		// exec simulation
-		// listen to modification in a directory
-		// send result
-		
-	}else{
-		res.writeHead(404);
-		res.end('404');
-	}
-});
-
-server.listen(9090, function(){
-	console.log('HTTP Server listen on 9090');
+io.on('connect', function(){
+	console.log('Connected to middleware')
+	// Ecouter le socket simulate
+	// Faire la simulation
+	// Envoyer le socket simulation_response
 });
