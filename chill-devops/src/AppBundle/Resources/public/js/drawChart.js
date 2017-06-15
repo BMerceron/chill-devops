adccbd5928aec54bc8282c8ea689337db9627a41
François Plet <francisco_canari@hotmail.fr>
2017-06-15 16:42:23 +0200

    13-showResult: Auto stash before merge of "SPB-13-showResult" and "origin/SPB-13-showResult"
    add buyingCose on chart

--- chill-devops/src/AppBundle/Resources/public/js/drawChart.js
+++ chill-devops/src/AppBundle/Resources/public/js/drawChart.js
@@ -2,29 +2,32 @@ $( document ).ready(function() {
     var data = $("#chartDatas").val();
 
     var dataset = JSON.parse(data);
-    console.log(dataset);
-
-    var dataTEST = [{Client:5000,PriceByMonth:20,BuyingCost:850,LastMonth:12},
-                    {Client:5500,PriceByMonth:20,BuyingCost:0,LastMonth:24},
-                    {Client:6050,PriceByMonth:20,BuyingCost:0,LastMonth:36},
-                    {Client:6655,PriceByMonth:50,BuyingCost:1200,LastMonth:48},
-                    {Client:7321,PriceByMonth:100,BuyingCost:0,LastMonth:60}];
 
 
   var chart = c3.generate({
   bindto: "#chart-container",
-  data:/*{
-    json: data,
-    keys: {
-      value: ['Client', 'PriceByMonth'],
-        }
-    }*/{
-            json: dataset,
-            keys: {
-//                x: 'name', // it's possible to specify 'x' when category axis
-                value: ['Client', 'PriceByMonth'],
-            }
+  data:{
+        json: dataset,
+        keys: {
+            value: ['Client', 'PriceByMonth', 'BuyingCost'],
         },
+        types: {
+            BuyingCost: 'bar',
+        },
+        axes: {
+            Client: 'y',
+            BuyingCost: 'y2'
+        }
+    },
+    bar: {
+        width: {
+            ratio: 0.5 }
+    },
+    axis: {
+        y2: {
+            show: true
+        }
+    }
   });
 
 });
