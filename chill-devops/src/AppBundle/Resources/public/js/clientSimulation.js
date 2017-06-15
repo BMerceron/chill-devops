f12f37be5f9a85ff5b310041697114b1f99c55ad
Bastoon <bastien.merceron@gmail.com>
2017-06-15 11:56:02 +0200

    Feat api client side

---
+++ chill-devops/src/AppBundle/Resources/public/js/clientSimulation.js
@@ -0,0 +1,14 @@
+/**
+ * Created by devmercerie on 15/06/17.
+ */
+
+var socket = io.connect('http://192.170.1.14:9090');
+
+function onSimulate() {
+    debugger;
+    socket.emit('simulate', 'simulate');
+}
+
+socket.on('response', function(data) {
+    console.log(data);
+})
