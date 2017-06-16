d1f6a10bb5618ba152c065ee3b98ebe28e932df1
Nicolas Ferec <nicolasferec@outlook.com>
2017-06-16 11:41:28 +0200

    trying to fix server-api

--- server-api/app.js
+++ server-api/app.js
@@ -7,38 +7,37 @@ var exec = require('child_process').exec;
 socket.on('simulate', function(id){
 
 	var name = 'Test server';
+	var idpath = id.replace('_', '').replace('-', '').toLowerCase();
 
 	exec('echo "'+id+'" | /chill_project/scripts/launch_test.sh', function(error, stdout, stderr) {
-		setTimeout(function(){
-			fs.readFile('/var/lib/phoronix-test-suite/test-results/'+id+'/composite.xml', 'utf-8', function(err, data){
-				if(err)
-					console.log(err);
-				
-				var json = JSON.parse(parser.toJson(data));
-				var hardware = json.PhoronixTestSuite.System.Hardware;
-				var re = /Hz \((.+)\), Motherboard/g;
-				var core = re.exec(hardware)[1];
-				re = /Memory: ([\d]) x (.+) MB DRAM/g;
-				var ramtemp = re.exec(hardware);
-				var ram = ramtemp[1] * ramtemp[2];
-				re = /Disk: ([\d]+)GB/g;
-				var disk = re.exec(hardware)[1];
+		fs.readFile('/var/lib/phoronix-test-suite/test-results/'+idpath+'/composite.xml', 'utf-8', function(err, data){
+			if(err)
+				console.log(err);
 
-				var result = {
-					capacity: json.PhoronixTestSuite.Result.Data.Entry.Value,
-					config: {
-						name: name,
-						core: core,
-						ram: ram,
-						disk: disk 
-					}
-					
+			var json = JSON.parse(parser.toJson(data));
+			var hardware = json.PhoronixTestSuite.System.Hardware;
+			var re = /Hz \((.+)\), Motherboard/g;
+			var core = re.exec(hardware)[1];
+			re = /Memory: ([\d]) x (.+) MB DRAM/g;
+			var ramtemp = re.exec(hardware);
+			var ram = ramtemp[1] * ramtemp[2];
+			re = /Disk: ([\d]+)GB/g;
+			var disk = re.exec(hardware)[1];
+
+			var result = {
+				capacity: json.PhoronixTestSuite.Result.Data.Entry.Value,
+				config: {
+					name: name,
+					core: core,
+					ram: ram,
+					disk: disk 
 				}
 				
-				socket.emit('response', result);
+			}
+			
+			socket.emit('response', result);
 
-			});
-		}, 500);
+		});
 	});
 
 });
