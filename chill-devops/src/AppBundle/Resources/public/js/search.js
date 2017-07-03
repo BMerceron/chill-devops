f1b98b3f751b6d2061ccc746ba097437a25f36bd
jonathandodard <jonathan.dodard@gmail.com>
2017-07-03 15:45:26 +0200

    create search bar

---
+++ chill-devops/src/AppBundle/Resources/public/js/search.js
@@ -0,0 +1,32 @@
+/**
+ * Created by jonathan on 03/07/17.
+ */
+$('.k-js-input-search').keyup(function () {
+    if ($(this).val().length < 2) {
+        $('.k-js-result-search-scenario li').remove()
+    }
+    if ($(this).val().length > 2) {
+        $.ajax({
+            url: "/scenario/search",
+            data: {'data': $(this).val()},
+            success: function (data) {
+                for (var counter = 0; counter < data.length; counter ++){
+                    var id = 'li-result-'+data[counter].id;
+                    var href = '/scenario/show/'+data[counter].id;
+                    if (!document.getElementById(id)) {
+                        $('.k-js-result-search-scenario').append('<li id="'+id+'" class="collection-item text-white"><a href='+href+'>'+data[counter].name+'</a></li>');
+                    }
+                    $( ".collection-item" ).hover(
+                        function() {
+                            $( this ).addClass( "active" );
+                        }, function() {
+                            $( this ).removeClass( "active" );
+                        }
+                    );
+                }
+
+            }
+        })
+    }
+
+});
