390e6c9e260165215d28632f51532029126e8a7a
Nicolas Ferec <nicolasferec@outlook.com>
2017-07-05 12:05:48 +0200

    Working on real waiting time

--- chill-devops/src/AppBundle/Resources/public/js/clientSimulation.js
+++ chill-devops/src/AppBundle/Resources/public/js/clientSimulation.js
@@ -2,18 +2,17 @@
  * Created by devmercerie on 15/06/17.
  */
 
+
 var socket = io.connect('http://192.170.1.14:9090');
 
 function getWaiting(){
-    console.log('getWaiting');
     socket.emit('waiting');
 };
 
 socket.on('waiting', function(waiting){
-   console.log(waiting);
-   alert(waiting);
+    console.log(waiting);
 });
 
 socket.on('response', function(results) {
     console.log(results);
-})
+});
